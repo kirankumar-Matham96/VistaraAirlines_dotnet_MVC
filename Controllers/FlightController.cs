9ce53ad45cb259ae31143657806238a0387cca4d
Kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2026-05-06 12:02:04 +0530

    feat: added flight model, controller, and views

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VistaraAirLinesApp.Models;
+using VistaraAirLinesApp.Models.ViewModels;
+
+namespace VistaraAirLinesApp.Controllers
+{
+    public class FlightController : Controller
+    {
+        VISTARA_DBEntities4 db;
+
+        public FlightController() {
+            db = new VISTARA_DBEntities4();
+        }
+
+        [NonAction]
+        public void GetTimeRanges()
+        {
+            TempData["HHRange"] = Enumerable.Range(0, 24).ToList();
+            TempData["MMRange"] = Enumerable.Range(0, 60).ToList();
+            TempData["SSRange"] = Enumerable.Range(0, 60).ToList();
+            
+            //TempData.Keep("HHRange");
+            //TempData.Keep("MMRange");
+            //TempData.Keep("SSRange");
+        }
+
+        // GET: Flight
+        public ActionResult AddFlight()
+        {
+            GetTimeRanges();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddFlight(FlightViewModel flight)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // do something here
+                }
+
+                GetTimeRanges();
+                return View(flight);
+            }
+            catch (Exception ex) {
+                //ModelState.AddModelError("", "Something went wrong while adding flight");
+                ModelState.AddModelError("", ex.Message);
+                return View(flight);
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/FlightViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VistaraAirLinesApp.Models.ViewModels
+{
+    public class FlightViewModel
+    {
+        [Required]
+        [Display(Name = "Flight Code")]
+        public string FlightCode { get; set; }
+        
+        [Required]
+        [Display(Name = "Flight Name")]
+        public string FlightName { get; set; }
+
+        [Required]
+        public string Source { get; set; }
+
+        [Required] 
+        public string Destination { get; set; }
+
+        //public TimeSpan ArrivalTime { get; set; }
+        [Required]
+        public TimeSpan ArrivalHrs { get; set; }
+        [Required]
+        public TimeSpan ArrivalMin { get; set; }
+        [Required]
+        public TimeSpan ArrivalSec { get; set; }
+        [Required]
+        public TimeSpan ArrivalAmpm { get; set; }
+
+
+        //public TimeSpan DepartureTime { get; set; }
+        [Required]
+        public TimeSpan DepartureHrs { get; set; }
+        [Required]
+        public TimeSpan DepartureMin { get; set; }
+        [Required]
+        public TimeSpan DepartureSec { get; set; }
+        [Required]
+        public TimeSpan DepartureAmpm { get; set; }
+
+        [Required]
+        [Display(Name = "Executive Seats")]
+        public int ExecutiveSeats { get; set; }
+        
+        [Required]
+        [Display(Name = "Executive Fare")]
+        public double ExecutiveFare { get; set; }
+
+        [Required] 
+        [Display(Name = "Business Seats")]
+        public int BusinessSeats { get; set; }
+
+        [Required] 
+        [Display(Name = "Business Fare")]
+        public double BusinessFare { get; set; }
+
+        [Required] 
+        [Display(Name = "Economy Seats")]
+        public int EconomySeats { get; set; }
+
+        [Required] 
+        [Display(Name = "Economy Fare")]
+        public double EconomyFare { get; set; }
+    }
+}
